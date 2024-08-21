ac2460bd839809c94a35a26ca49b1dab8b801377
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2024-08-21 16:55:32 +0530

    context

--- src/Components/context.jsx
+++ src/Components/context.jsx
@@ -3,27 +3,40 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 
-export const ContextContainer = createContext()
+export const ContextContainer = createContext();
 
-const MyContextProvider = ({children}) => {
-    const [name, setName] = useState(()=> localStorage.getItem("math-game-react"))
-    const [globalScore, setGlobalScore] = useState(0)
-    const [globalHighScore, setGlobalHighScore] = useState(0)
-    const [completedLevelName, setCompletedLevelName] = useState("")
+const MyContextProvider = ({ children }) => {
+  const [name, setName] = useState(() =>
+    localStorage.getItem("math-game-react")
+  );
+  const [globalScore, setGlobalScore] = useState(0);
+  const [globalHighScore, setGlobalHighScore] = useState(0);
+  const [completedLevelName, setCompletedLevelName] = useState("");
 
-    function setNameFunction(playersName){
-        setName(playersName)
-    }
+  function setNameFunction(playersName) {
+    setName(playersName);
+  }
 
-    return(
-        <ContextContainer.Provider value={{name, setNameFunction, globalScore, setGlobalScore, globalHighScore, setGlobalHighScore, completedLevelName, setCompletedLevelName}}>
-            {children}
-        </ContextContainer.Provider>
-    )
+  return (
+    <ContextContainer.Provider
+      value={{
+        name,
+        setNameFunction,
+        globalScore,
+        setGlobalScore,
+        globalHighScore,
+        setGlobalHighScore,
+        completedLevelName,
+        setCompletedLevelName,
+      }}
+    >
+      {children}
+    </ContextContainer.Provider>
+  );
+};
+
+export function useAuth() {
+  return useContext(ContextContainer);
 }
 
-export function useAuth(){
-    return useContext(ContextContainer)
-}
-
-export default MyContextProvider
+export default MyContextProvider;

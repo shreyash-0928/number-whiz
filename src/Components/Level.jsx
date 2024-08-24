c1a3a009c058ead9f5fef8d34416f72bd8545eb7
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2024-08-24 10:53:24 +0530

    Level section

--- src/Components/Level.jsx
+++ src/Components/Level.jsx
@@ -1,34 +1,26 @@
-import React from 'react'
-import { useRef, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Level({levelName, levelDisplay}) {
-    const navigate = useNavigate()
-    const btnRef = useRef()
+function Level({ levelName, levelDisplay }) {
+  const navigate = useNavigate();
+  const btnRef = useRef();
 
-
-    function handleClick(name){
-        btnRef.current.classList.add(`active-${name}`)
-        setTimeout(() => {
-             navigate(`/category/${levelName}`)
-        }, 500);
-       
-    }
-
+  function handleClick(name) {
+    btnRef.current.classList.add(`active-${name}`);
+    setTimeout(() => {
+      navigate(`/category/${levelName}`);
+    }, 500);
+  }
 
   return (
-    <div 
-    onClick={()=>handleClick(levelName)}
-    className={`${levelName}`}>
-
-        <div
-        ref={btnRef}
-        className={`${levelName}-circle`}>
-            <p>{levelDisplay}</p>
-            </div>
-                <p>{levelName}</p>
-            </div>
-  )
+    <div onClick={() => handleClick(levelName)} className={`${levelName}`}>
+      <div ref={btnRef} className={`${levelName}-circle`}>
+        <p>{levelDisplay}</p>
+      </div>
+      <p>{levelName}</p>
+    </div>
+  );
 }
 
-export default Level
+export default Level;
